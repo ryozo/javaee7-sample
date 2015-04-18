3242ad13541e76771ba17e47bbb582698ca0429c
Ryozo Watanabe <clearstor@gmail.com>
2015-04-18 16:53:21 +0900

    ユーザ情報をRepositoryから取得するよう変更

--- src/main/java/net/equj65/javaee7/example/repository/UserRepository.java
+++ src/main/java/net/equj65/javaee7/example/repository/UserRepository.java
@@ -1,8 +1,31 @@
 package net.equj65.javaee7.example.repository;
 
+import net.equj65.javaee7.example.domain.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 /**
  * ユーザ情報を提供するRepositoryです。
  * @author ryozo
  */
+@ApplicationScoped
 public class UserRepository {
+    // TODO DBに変える。
+    private final ConcurrentMap<String, User> userMap = new ConcurrentHashMap<String, User>() {{
+        put("id1", new User("id1", "user1", 20, "user1@example.com", "address1"));
+        put("id2", new User("id2", "user2", 20, "user2@example.com", "address2"));
+        put("id3", new User("id3", "user3", 20, "user3@example.com", "address3"));
+        put("id4", new User("id4", "user4", 20, "user4@example.com", "address4"));
+        put("id5", new User("id5", "user5", 20, "user5@example.com", "address5"));
+        put("id6", new User("id6", "user6", 20, "user6@example.com", "address6"));
+    }};
+
+    public List<User> findAll() {
+        return new ArrayList<>(userMap.values());
+    }
+
 }
--- src/main/java/net/equj65/javaee7/example/service/impl/UserServiceImpl.java
+++ src/main/java/net/equj65/javaee7/example/service/impl/UserServiceImpl.java
@@ -1,10 +1,11 @@
 package net.equj65.javaee7.example.service.impl;
 
 import net.equj65.javaee7.example.domain.User;
+import net.equj65.javaee7.example.repository.UserRepository;
 import net.equj65.javaee7.example.service.UserService;
 
 import javax.ejb.Stateless;
-import java.util.ArrayList;
+import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -14,16 +15,11 @@ import java.util.List;
 @Stateless
 public class UserServiceImpl implements UserService {
 
+    @Inject
+    UserRepository userRepository;
+
     @Override
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<User>() {{
-            add(new User("id1", "user1", 20, "user1@example.com", "address1"));
-            add(new User("id2", "user2", 20, "user2@example.com", "address2"));
-            add(new User("id3", "user3", 20, "user3@example.com", "address3"));
-            add(new User("id4", "user4", 20, "user4@example.com", "address4"));
-            add(new User("id5", "user5", 20, "user5@example.com", "address5"));
-            add(new User("id6", "user6", 20, "user6@example.com", "address6"));
-        }};
-        return userList;
+        return userRepository.findAll();
     }
 }
